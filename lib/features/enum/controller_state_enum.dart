5f09617ca69127ac4f744b795b03be08fa42b944
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-21 14:10:20 +0100

    added enum

---
+++ lib/features/enum/controller_state_enum.dart
@@ -0,0 +1,1 @@
+enum ControllerState { init, busy, success, error }
--- lib/features/home/binding/home_bindging.dart
+++ lib/features/home/binding/home_bindging.dart
@@ -1,9 +1,15 @@
 import 'package:get/get.dart';
+import 'package:weather_app/features/home/controller/get_weather_controller.dart';
 import 'package:weather_app/features/home/controller/home_controller.dart';
+import 'package:weather_app/features/home/service/weather_service.dart';
 
 class HomeBindings extends Bindings {
   @override
   void dependencies() {
     Get.put<HomeController>(HomeController());
+    Get.put<WeatherService>(WeatherService.create());
+    Get.put<GetOneCallWeatherController>(GetOneCallWeatherController());
   }
+
+  
 }
