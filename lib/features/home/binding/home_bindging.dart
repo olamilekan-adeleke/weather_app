3d67f0cfe5c24351abfc09c5db9abc9392231ef6
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-21 18:29:32 +0100

    update

--- lib/features/home/binding/home_bindging.dart
+++ lib/features/home/binding/home_bindging.dart
@@ -3,7 +3,7 @@ import 'package:weather_app/features/home/controller/get_weather_controller.dart
 import 'package:weather_app/features/home/controller/home_controller.dart';
 import 'package:weather_app/features/home/service/reverse_geocoding_service.dart';
 import 'package:weather_app/features/home/service/weather_service.dart';
-import 'package:weather_app/features/notiification/service/local_database_service.dart';
+import 'package:weather_app/features/notiification/controller/notifcation_controller.dart';
 
 class HomeBindings extends Bindings {
   @override
@@ -12,6 +12,6 @@ class HomeBindings extends Bindings {
     Get.put<WeatherService>(WeatherService.create());
     Get.put<ReverseGeocodingService>(ReverseGeocodingService());
     Get.put<GetOneCallWeatherController>(GetOneCallWeatherController());
-    Get.put<LocalDatabaseService>(LocalDatabaseService());
+    Get.put<NotificationController>(NotificationController());
   }
 }
---
+++ lib/features/notiification/controller/notifcation_controller.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+import 'package:weather_app/features/home/enum/controller_state_enum.dart';
+import 'package:weather_app/features/home/model/one_call_weather_moder.dart';
+import 'package:weather_app/features/notiification/service/local_database_service.dart';
+
+class NotificationController extends GetxController {
+  final Rx<ControllerState> controllerState = ControllerState.init.obs;
+  static final LocalDatabaseService localDatabaseService =
+      LocalDatabaseService();
+  final RxList<WeatherModel> notifications = <WeatherModel>[].obs;
+
+  Future<void> getNotifications() async {
+    final List<Map<String, dynamic>> data =
+        await localDatabaseService.getNotificationData();
+
+    notifications.value = data.map((e) => WeatherModel.fromMap(e)).toList();
+    controllerState.value = ControllerState.success;
+  }
+
+  void saveNotifications(Map<String, dynamic> data) {}
+
+  Future<void> init() async {
+    controllerState.value = ControllerState.busy;
+    await localDatabaseService.openNotificationsBox();
+    getNotifications();
+  }
+
+  @override
+  void onReady() {
+    init();
+    super.onReady();
+  }
+}
