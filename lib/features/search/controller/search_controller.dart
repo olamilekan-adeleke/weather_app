3b16ae78926f09da83fc6ff598e63be34d572385
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-22 03:42:17 +0100

    fixes

---
+++ lib/features/search/controller/search_controller.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:weather_app/features/home/enum/controller_state_enum.dart';
+
+class SearchController extends GetxController {
+
+  Future<void> searchForLocation(String username) async {
+    // if (username.isEmpty || searchQuery.value == '') return;
+
+    // try {
+    //   controllerState.value = ControllerState.busy;
+
+    //   controllerState.value = ControllerState.success;
+    // } catch (e) {
+    //   // errorAlertBadge(
+    //   //   'Something went wrong while searching for users. \nPlease try again!',
+    //   // );
+    // }
+
+    // searchQuery.value = '';
+  }
+
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,11 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:sizer/sizer.dart';
 import 'package:weather_app/features/home/views/screen/home_page.dart';
-
-
 import 'features/home/binding/home_bindging.dart';
-import 'features/notiification/service/back_ground_worker_service.dart';
 
 Future<void> main() async {
   await Hive.initFlutter();
   WidgetsFlutterBinding.ensureInitialized();
-  await BackgroundWorkerService.init();
   runApp(const MyApp());
 }
 
@@ -47,3 +43,5 @@ List<GetPage<dynamic>>? pages() {
     
   ];
 }
+
+
