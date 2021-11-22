3dc8c8deb08760cc669d338640ee6b1b85dd0a5e
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-22 09:11:33 +0100

    fix

--- lib/features/home/views/screen/home_page.dart
+++ lib/features/home/views/screen/home_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:weather_app/cores/components/custom_scaffold_widget.dart';
+import 'package:weather_app/cores/components/shimmer_widget.dart';
 import 'package:weather_app/cores/utils/custom_sizer_utils.dart';
 import 'package:weather_app/features/home/controller/get_weather_controller.dart';
+import 'package:weather_app/features/home/enum/controller_state_enum.dart';
 import 'package:weather_app/features/home/views/widgets/home_forecast_button_widget.dart';
 import 'package:weather_app/features/home/views/widgets/home_header_widget.dart';
 import 'package:weather_app/features/home/views/widgets/home_weather_details_widget.dart';
@@ -33,6 +35,14 @@ class HomeScreen extends StatelessWidget {
                   SizedBox(
                     height: sizerHeight(20),
                     child: Obx(() {
+                      if (getOneCallWeatherController.controllerState.value ==
+                          ControllerState.busy) {
+                        return shimmerRectangle(
+                          height: sizerSp(30),
+                          width: double.infinity,
+                        );
+                      }
+
                       return MapWidget(
                         LatLng(
                           getOneCallWeatherController.weatherModel?.value.lat ??
--- lib/features/search/views/screen/selected_location_screen.dart
+++ lib/features/search/views/screen/selected_location_screen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:weather_app/cores/components/custom_scaffold_widget.dart';
+import 'package:weather_app/cores/components/shimmer_widget.dart';
 import 'package:weather_app/cores/utils/custom_sizer_utils.dart';
+import 'package:weather_app/features/home/enum/controller_state_enum.dart';
+import 'package:weather_app/features/map/map_widget.dart';
 import 'package:weather_app/features/search/controller/search_controller.dart';
 import 'package:weather_app/features/search/views/widgets/search_header_widget.dart';
 import 'package:weather_app/features/search/views/widgets/search_weather_details_widget.dart';
 
 class SelectedLocationScreen extends StatelessWidget {
   const SelectedLocationScreen({Key? key}) : super(key: key);
+
+  static final SearchController searchController = Get.find<SearchController>();
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +25,30 @@ class SelectedLocationScreen extends StatelessWidget {
             height: sizerHeight(90),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const <Widget>[
-                SearchHeaderWidget(),
-                SearchWeatherDetailsWidget(),
-                SizedBox(),
-                // HomeForecastButtonWidget(),
+              children: <Widget>[
+                const SearchHeaderWidget(),
+                const SearchWeatherDetailsWidget(),
+                SizedBox(
+                  height: sizerHeight(20),
+                  child: Obx(() {
+                    if (searchController.controllerState.value ==
+                        ControllerState.busy) {
+                      return shimmerRectangle(
+                        height: sizerSp(30),
+                        width: double.infinity,
+                      );
+                    }
+
+                    return MapWidget(
+                      LatLng(
+                        searchController.weatherModel?.value.coord?.lat ?? 0,
+                        searchController.weatherModel?.value.coord?.lon ?? 0,
+                      ),
+                      placeName: searchController.weatherModel?.value.cityName,
+                    );
+                  }),
+                ),
+                const SizedBox(),
               ],
             ),
           ),
